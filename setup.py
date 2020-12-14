6ad0584091a5b8c35960e301bd52185a3396fcbb
Dominik S. Buse <buse@ccs-labs.org>
2020-12-14 09:13:43 +0100

    Release v0.1.0 to pypi.

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import setup
 
-setup(name='veins_gym',
-      version='0.0.1',
-      install_requires=['gym', 'zmq', 'protobuf'],
-      packages=['veins_gym'],
-      package_dir={'': 'src'},
-      data_files=[('protobuf', ['protobuf/veinsgym.proto'])],
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="veins_gym",
+    version="0.1.0",
+    author="Dominik S. Buse",
+    author_email="buse@ccs-labs.org",
+    description="Reinforcement Learning-based VANET simulations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www2.tkn.tu-berlin.de/software/veins-gym/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    package_dir={'': 'src'},
+    data_files=[('protobuf', ['protobuf/veinsgym.proto'])],
+    packages=['veins_gym'],
 )
